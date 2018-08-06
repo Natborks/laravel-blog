bd7395537aaf23054c3a2652af89fb64d2b0397d
bk <natborks@gmail.com>
2018-08-06 04:04:09 -0400

    add post views index create

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.layout')
+
+@section('content')
+
+
+<div class="col-sm-8 blog-main">
+
+	<form>
+		  <div class="form-group">
+		    <label for=""></label>
+		    <input type="" class="form-control" id="" aria-describedby="" placeholder="">
+		    <small id="" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		  </div>
+		  <div class="form-group">
+		    <label for="">Password</label>
+		    <input type="" class="form-control" id="" placeholder="">
+		  </div>
+
+		  <button type="submit" class="btn btn-primary"></button>
+	</form>
+</div><!-- /.blog-main -->
+
+@endsection
